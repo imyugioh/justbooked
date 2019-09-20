25e1f322b697f379799d9f897f14fb3be9da3504
Raptor <blueraptor1001@gmail.com>
2019-09-20 05:01:19 -0700

    update the api controller

--- app/controllers/api/menus_controller.rb
+++
@@ -1,8 +0,0 @@
-module Api
-  class MenusController < ApplicationController
-    def show
-      @menu = Menu.find_by_id(params[:id])
-      render json: @menu, include: [:menu_items, :addons], status: :ok
-    end
-  end
-end
